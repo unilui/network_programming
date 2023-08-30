09eac321f28e47c0391081b401aeb45f0c01f23d
Felipe Camargo <lufelip2@student.42sp.org.br>
2023-08-30 19:53:50 -0300

    Complete implementation

--- tcp.cpp
+++ tcp.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
-#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
 
-#define SERVER_PORT 80
-#define MAX_LINE 4096
+int main() {
+	const char *hostname = "www.google.com";
+	const char *http_request = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
 
-int main(int argc, char **argv)
-{
-    struct sockaddr skad;
-    
-    int sockfd, n;
-    int sendbytes;
-    struct sockaddr_in servaddr; 
+	// Create a socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		perror("Socket creation failed");
+		return 1;
+	}
 
-    if (argc != 2)
-    {
-        std::cout << "Usage: ./tcp <URL>" << std::endl;
-        return (1);
-    }
+	// Get IP address of the hostname using getaddrinfo
+	struct addrinfo hints, *result;
+	std::memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
 
+	int addr_result = getaddrinfo(hostname, "http", &hints, &result);
+	if (addr_result != 0) {
+		std::cerr << "getaddrinfo: " << gai_strerror(addr_result) << std::endl;
+		return 1;
+	}
 
+	// Connect to the remote server
+	if (connect(sock, result->ai_addr, result->ai_addrlen) == -1) {
+		perror("Connect failed");
+		freeaddrinfo(result);
+		return 1;
+	}
 
-    std::cout << "Site: " << std::string(argv[1]) << std::endl;
-    return (0);
+	// Send the HTTP request
+	if (send(sock, http_request, strlen(http_request), 0) == -1) {
+		perror("Send failed");
+		freeaddrinfo(result);
+		close(sock);
+		return 1;
+	}
+
+	// Receive and print the response
+	char buffer[1024];
+	ssize_t bytes_received;
+	while ((bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+		buffer[bytes_received] = '\0';
+		std::cout << buffer;
+	}
+
+	// Clean up
+	freeaddrinfo(result);
+	close(sock);
+
+	return 0;
 }
